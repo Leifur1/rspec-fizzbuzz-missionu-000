45153684c0cdb454a8881fb51472c6af850575f5
Leifur <leifurerik@gmail.com>
2018-09-26 22:53:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 5 == 0) && (int % 3 == 0)
-    "Fizz"
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
-    "FizzBuzz"
+    "Fizz"
   else
     nil
   end
